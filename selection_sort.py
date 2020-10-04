9bbae883264e696022eb7cd583d9c248daadc41f
pinglu85 <luping8576@hotmail.com>
2020-10-04 03:19:43 +0200

    Refactor the code

--- selection_sort.py
+++ selection_sort.py
@@ -1,19 +1,15 @@
-def find_largest(arr):
-    largest = arr[0]
-    largest_index = 0
-    for i in range(1, len(arr)):
-        if arr[i] > largest:
-            largest = arr[i]
-            largest_index = i
-    return largest_index
-
-
 def selection_sort(arr):
-    sorted_arr = []
     for i in range(len(arr)):
-        largest = find_largest(arr)
-        sorted_arr.append(arr.pop(largest))
-    return sorted_arr
+        lowest = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[lowest]:
+                lowest = j
+        # swap
+        if i != lowest:
+            temp = arr[lowest]
+            arr[lowest] = arr[i]
+            arr[i] = temp
+    return arr
 
 
 print(selection_sort([5, 3, 6, 2, 10]))
